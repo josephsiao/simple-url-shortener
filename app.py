6922524c0d4bf8ba24ef019387c7118e3ef598a4
Joseph <joseph.siaojh@gmail.com>
2019-10-09 14:45:36 +0800

    Add check_data_exist and url_redirect

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from configparser import ConfigParser
 
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request
 
 from pg_database import PostgreDB
 from short_url_generator import URLShortener
@@ -22,14 +22,29 @@ POSTGRE_DB = PostgreDB(DB)
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        url = request.values['url']
-        short_url = URL_SHORTENER.short(url)
-
-        POSTGRE_DB.add_data((short_url, url))
-
-        return PROTOCOL + '://' + DOMAIN_NAME + '/' + short_url
+        url = request.values['url'].strip()
+        short_url = POSTGRE_DB.check_data_exist(original=url)
+        if not short_url:
+            short_url = URL_SHORTENER.short(url)
+            if not short_url:
+                result = 'ValidationFailure. This may be an incorrect URL.'
+            else:
+                POSTGRE_DB.add_data((short_url, url))
+                result = PROTOCOL + '://' + DOMAIN_NAME + '/' + short_url
+        else:
+            result = PROTOCOL + '://' + DOMAIN_NAME + '/' + short_url
+        return render_template('index.html', url=url, result=result)
     else:
         return render_template('index.html')
+
+
+@app.route('/<url>')
+def url_redirect(url):
+    result = POSTGRE_DB.check_data_exist(shorten=url)
+    if result:
+        return redirect(result)
+    else:
+        return 'PAGE DO NOT EXIST.'
 
 
 if __name__ == '__main__':
--- pg_database.py
+++ pg_database.py
@@ -11,12 +11,37 @@ class PostgreDB():
             cur = conn.cursor()
             cur.execute(
                 'INSERT INTO URL (SHORTEN, ORIGINAL) VALUES (%s, %s)', data)
+            conn.commit()
         except Exception as e:
             conn.rollback()
             raise e
         finally:
             cur.close()
-            conn.commit()
+            conn.close()
+
+    def check_data_exist(self, shorten=None, original=None):
+        try:
+            conn = psycopg2.connect(**self.database_config)
+            cur = conn.cursor()
+            if shorten:
+                cur.execute('SELECT * FROM URL WHERE SHORTEN = %s',
+                            (shorten,))
+            else:
+                cur.execute('SELECT * FROM URL WHERE ORIGINAL = %s',
+                            (original,))
+            result = cur.fetchone()
+            if result and original:
+                return result[1]
+            elif result and shorten:
+                return result[2]
+            else:
+                return None
+        except Exception as e:
+            conn.rollback()
+            raise e
+        finally:
+            cur.close()
+            conn.close()
 
     def create_table(self):
         try:
@@ -30,9 +55,10 @@ class PostgreDB():
                         ORIGINAL          TEXT    NOT NULL);
                 '''
             )
+            conn.commit()
         except Exception as e:
             conn.rollback()
             raise e
         finally:
             cur.close()
-            conn.commit()
+            conn.close()
